b19bfb3d1f35d7ec3d62885f1acb9e395d7e4014
filipecfv <107270778+filipecfv@users.noreply.github.com>
2023-04-30 16:35:25 -0300

    history file functions

---
+++ src/history.py
@@ -0,0 +1,34 @@
+import os 
+import pandas as pd
+
+from datetime import date, datetime 
+from time import sleep
+
+path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'statistics'))
+
+def history(time_work):
+    today = str(date.today())
+    history = pd.read_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year)) 
+    history.loc[len(history)] = [today,
+                                 1,
+                                 float('{0:.2f}'.format(time_work/60))]
+    history = history.groupby('Date').sum()
+    history.to_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year), index = True)
+
+def check_for_history_file():
+    try: 
+        pd.read_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year))
+    except FileNotFoundError:
+        print("creating a history file...")
+        create_history_file()
+
+def create_history_file():
+    sleep(2)
+    current_year = datetime.now().year
+    dates = pd.date_range(
+            start = f'{current_year}-01-01', 
+            end = f'{current_year}-12-31').strftime('%Y-%m-%d').tolist()
+    file = pd.DataFrame(columns = ['Date', 'Pomodoros', 'Work Hours'])
+    file['Date'] = dates
+    file.to_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year),
+        index = False)
