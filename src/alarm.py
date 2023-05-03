ebfda444847db88f6a8f7ddd359aaeb40991e52a
filipecfv <107270778+filipecfv@users.noreply.github.com>
2023-05-03 14:51:14 -0300

    improve code

--- src/alarm.py
+++ src/alarm.py
@@ -2,7 +2,8 @@ import sounddevice as sd
 import soundfile as sf
 import os
 
-path = os.path.dirname( __file__ )
+path = os.path.dirname(__file__)
+
 
 def alarm():
     data, samplerate = sf.read(path + '/sounds/alarm.ogg')
