d12b1cf4a6ffaa9e3052d5f7f62b1db4dff84044
filipecfv <107270778+filipecfv@users.noreply.github.com>
2023-05-03 14:52:17 -0300

    improve formatting

--- src/clock.py
+++ src/clock.py
@@ -1,7 +1,8 @@
-from time import sleep 
+from time import sleep
+
 
 def clock(time):
-    for i in range(0, time): 
+    for i in range(0, time):
         for n in range(0, 60):
-            print(">>>", i, ":", n, end = "\r")
+            print(">>>", i, ":", n, end="\r")
             sleep(1)
