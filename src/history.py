97d032bc590ec5b1c8bfd2040b93aebf8b6ccbb7
filipecfv <107270778+filipecfv@users.noreply.github.com>
2023-05-03 14:59:38 -0300

    improve formatting

--- src/history.py
+++ src/history.py
@@ -1,34 +1,44 @@
-import os 
+import os
 import pandas as pd
 
-from datetime import date, datetime 
+from datetime import date, datetime
 from time import sleep
 
-path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'statistics'))
+path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..',
+            'statistics'
+            )
+        )
+
+history_file = '/plumodoro_history_{}.csv'.format(datetime.now().year)
+
 
 def history(time_work):
     today = str(date.today())
-    history = pd.read_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year)) 
+    history = pd.read_csv(path + history_file)
     history.loc[len(history)] = [today,
                                  1,
                                  float('{0:.2f}'.format(time_work/60))]
     history = history.groupby('Date').sum()
-    history.to_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year), index = True)
+    history.to_csv(path + history_file, index=True)
+
 
 def check_for_history_file():
-    try: 
-        pd.read_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year))
+    try:
+        pd.read_csv(path + history_file)
     except FileNotFoundError:
         print("creating a history file...")
         create_history_file()
+
 
 def create_history_file():
     sleep(2)
     current_year = datetime.now().year
     dates = pd.date_range(
-            start = f'{current_year}-01-01', 
-            end = f'{current_year}-12-31').strftime('%Y-%m-%d').tolist()
-    file = pd.DataFrame(columns = ['Date', 'Pomodoros', 'Work Hours'])
+            start=f'{current_year}-01-01',
+            end=f'{current_year}-12-31').strftime('%Y-%m-%d').tolist()
+    file = pd.DataFrame(columns=['Date', 'Pomodoros', 'Work Hours'])
     file['Date'] = dates
-    file.to_csv(path + '/plumodoro_history_{}.csv'.format(datetime.now().year),
-        index = False)
+    file.to_csv(path + history_file, index=False)
